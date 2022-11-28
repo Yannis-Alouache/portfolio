54e11444fcdae4daaea31cd104c5ceb09fe3d20e
Yannis-Alouache <alouacehyannis1@gmail.com>
2022-11-28 09:59:06 +0100

    [+] added ContactMe personal info

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -1,5 +1,11 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { Container } from '@mui/system';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
 
 function ContactMe() {
 
@@ -18,18 +24,36 @@ function ContactMe() {
 
   return (
     <>
-        <section>
-            <h1>Contact Me</h1>
-
-            <form ref={form} onSubmit={sendEmail}>
-              <label>Name</label>
-              <input type="text" name="user_name" />
-              <label>Email</label>
-              <input type="email" name="user_email" />
-              <label>Message</label>
-              <textarea name="message" />
-              <input type="submit" value="Send" />
-            </form>
+        <section className="">
+          <Container className="self-center" maxWidth='lg'>
+            <Grid container spacing={4}>
+              <Grid className="square padding-2" xs={12} xl={4} sm={12}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                  <LocalPhoneOutlinedIcon />
+                  <h1 style={{color: 'white'}}>07.69.61.17.60</h1>
+                </Box>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                  <EmailOutlinedIcon />
+                  <h1 style={{color: 'white'}}>yannis.alouache@gmail.com</h1>
+                </Box>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                  <FmdGoodOutlinedIcon />
+                  <h1 style={{color: 'white'}}>Lille</h1>
+                </Box>
+              </Grid>
+              {/* <Grid xs={12} xl={9} sm={12}>
+                <form ref={form} onSubmit={sendEmail}>
+                  <label>Name</label>
+                  <input type="text" name="user_name" />
+                  <label>Email</label>
+                  <input type="email" name="user_email" />
+                  <label>Message</label>
+                  <textarea name="message" />
+                  <input type="submit" value="Send" />
+                </form>
+              </Grid> */}
+            </Grid>
+          </Container>
         </section>
     </>
   )
