7dd8490bb23d87e03dc01ed28e60bc7a329521f3
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-04 09:38:10 +0100

    [+] added link to github repo

--- src/Pages/ProjectDetail.js
+++ src/Pages/ProjectDetail.js
@@ -3,6 +3,8 @@ import Navbar from '../Components/Navbar'
 import Banner from '../Components/Banner'
 import Technologies from '../Components/Technologies'
 import Gallery from '../Components/Gallery'
+import { Link, Typography } from '@mui/material'
+import { Container } from '@mui/system'
 
 function ProjectDetail(props) {
   let {
@@ -18,9 +20,13 @@ function ProjectDetail(props) {
     <>
       <Navbar />
       <section className='projectDetail'>
-        <Banner name={name} description={description} />
+        <Banner name={name} description={description} link={link}/>
         <Technologies technologies={technologies} />
         <Gallery gallery={gallery} />
+        <Container  maxWidth="lg">
+          <Typography variant='h4'>Github</Typography>
+          <Link href='https://github.com/Yannis-Alouache/portfolio'>https://github.com/Yannis-Alouache/portfolio</Link>
+        </Container>
       </section>
     </>
   )
