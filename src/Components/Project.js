151650b83b19a300b6407843f3f6090c89a26a9d
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-05 13:06:02 +0100

    [+] linked project to its detail page

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,17 +1,20 @@
+import { Link } from '@mui/material'
 import React from 'react'
 
 function Project(props) {
-  const { name, tag, image } = props
+  const { name, tag, image, link } = props
 
   return (
     <>
-      <div className="project">
-        <div className='wrapper'>
-            <img className='image' src={require('../Assets/Images/' + image)} />
-            <h1>{name}</h1>
-            <p>{tag}</p>
+      <Link sx={{textDecoration: "none"}} href={link}>
+        <div className="project">
+          <div className='wrapper'>
+              <img className='image' alt="project" src={require('../Assets/Images/' + image)} />
+              <h1>{name}</h1>
+              <p>{tag}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   )
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
           <Grid container spacing={10}>
             {projects.map(project => (
               <Grid item xs={12} xl={6} sm={6} key={project.name}>
-                <Project name={project.name} tag={project.tag} image={project.image} />
+                <Project name={project.name} tag={project.tag} image={project.image} link={project.link}/>
               </Grid>
             ))}
           </Grid>
