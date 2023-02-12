5b992af393cfe0a2590eb067a133fd28a020b43d
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-12 23:55:12 +0100

    [+] replaced contact form

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser';
+import { Box, Container } from '@mui/system';
+import { Grid, Link, TextField, Typography, Button } from '@mui/material';
 
 function ContactMe() {
 
@@ -18,18 +20,37 @@ function ContactMe() {
 
   return (
     <>
-        <section>
-            <h1>Contact Me</h1>
-
-            <form ref={form} onSubmit={sendEmail}>
-              <label>Name</label>
-              <input type="text" name="user_name" />
-              <label>Email</label>
-              <input type="email" name="user_email" />
-              <label>Message</label>
-              <textarea name="message" />
-              <input type="submit" value="Send" />
-            </form>
+        <section className='contact'>
+          <Container maxWidth="lg">
+            <Grid container spacing={10}>
+              <Grid item xl={5}>
+                <Typography variant="h3">
+                  Get in Touch
+                </Typography>
+                <p>
+                 Thank you for taking the time to visit my portfolio. <br></br> If you have any questions or would like to know more about my projects or my background, please feel free to contact me by filling out the form on the right. <br></br> I will be happy to answer you as soon as possible.
+                </p>
+                <p className='mt30'>Don't like forms ?</p>
+                <p> 
+                  Email me at &nbsp;
+                  <Link href="mailto:yannisalouache@gmail.com">yannisalouache@gmail.com</Link>
+                </p>
+              </Grid>
+              <Grid item xl={7}>
+                <Box autoComplete="off">
+                  <form ref={form}>
+                    <TextField id="standard-basic" required className='input' sx={{width: "45%", marginRight: "10%"}} name="lastName" margin="dense" label="Last Name" variant="standard" />
+                    <TextField id="standard-basic" required className='input' sx={{width: "45%"}} name="firstName" margin="dense" label="First Name" variant="standard" />
+                    <TextField id="standard-basic" required className='input' sx={{width: "45%", marginRight: "10%"}} name="mail" margin="dense" label="E-mail" variant="standard" />
+                    <TextField id="standard-basic" required className='input' sx={{width: "45%"}} name="phone" margin="dense" label="Phone Number" variant="standard" />
+                    <TextField id="standard-basic" required className='input' sx={{width: "100%"}} name='message' margin="dense" label="Message" variant="standard" multiline rows={5} />
+                    <Button className='sendButton' onClick={sendEmail}>Envoyer</Button>
+                  </form>
+                </Box>
+                
+              </Grid>
+            </Grid>
+          </Container>
         </section>
     </>
   )
