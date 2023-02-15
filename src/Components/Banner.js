9a8de2fa17742e856c40907af3a02d28fdd522d0
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-15 14:07:41 +0100

    [+] added container

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import { Alert, Box, Grid, Typography } from '@mui/material'
 import { Link } from '@mui/material'
+import { Container } from '@mui/system'
 
 function Banner(props) {
     let {name, description, link} = props
     return (
         <Box className="self-center bannerWrapper" maxWidth='lg'>
-            <Grid container>
-                <Grid item className='text-centerv' md={12}>
-                    <Typography variant='h3'>
-                        {name}
-                    </Typography>
+            <Container>
+                <Grid container>
+                    <Grid item className='text-centerv' md={12}>
+                        <Typography variant='h3'>
+                            {name}
+                        </Typography>
+                    </Grid>
+                    <Grid item className='text-centerv' md={12}>
+                        <p>{description}</p>
+                    </Grid>
+                    <Grid item px={{paddingTop: "20px"}} className='text-centerv' md={12}>
+                        {link 
+                            ? <Link className='linkToProject' href={link}>{link}</Link>
+                        
+                            : <Alert px={{paddingRight: "20px"}} severity="error">
+                                Ce projet n'étant pas encore hébergé, il ne possède pas de lien
+                            </Alert>
+                        }
+                    </Grid>
                 </Grid>
-                <Grid item className='text-centerv' md={12}>
-                    <p>{description}</p>
-                </Grid>
-                <Grid item px={{paddingTop: "20px"}} className='text-centerv' md={12}>
-                    {link 
-                        ? <Link className='linkToProject' href={link}>{link}</Link>
-                    
-                        : <Alert px={{paddingRight: "20px"}} severity="error">
-                            Ce projet n'étant pas encore hébergé, il ne possède pas de lien
-                          </Alert>
-                    }
-                </Grid>
-            </Grid>
+            </Container>
         </Box>
     )
 }
