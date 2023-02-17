42a3a2cda92e299d0ff630569c2125277b283f3a
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-17 11:20:43 +0100

    [~] error handle

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -56,6 +56,9 @@ function ContactMe() {
           setMessage("")
           setIsLoading(false)
           form.current.reset()
+      })
+      .catch((error) => {
+        console.error(error)
       });
   };
 
